f146b44ea89c929d57709e7f321682908fd7c911
josephkem <josephkemoy@gmail.com>
2020-05-19 10:46:36 -0400

    Used bcrypt to encrypt the user's password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatr");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
@@ -48,6 +49,11 @@ router.post(
       });
 
       //Encrypted password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
 
       //Return jsonwebtoken
 
