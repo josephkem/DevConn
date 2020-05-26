033fb48124ca8c30a003ec1c06824d524b43b6a6
josephkem <josephkemoy@gmail.com>
2020-05-26 15:11:21 -0400

    Modified try/catch method to find user from the database

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,7 +10,15 @@ const User = require("../../models/User");
 //@access  Private
 router.get("/", auth, async (req, res) => {
   try {
-    const profile = await profile;
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populated("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
