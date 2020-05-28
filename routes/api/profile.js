c92859865fa29f80187e1d0f1e486243bc8437df
josephkem <josephkemoy@gmail.com>
2020-05-28 13:37:55 -0400

    Create a route which gets all the user profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -101,4 +101,20 @@ router.post(
     res.send("Hello");
   }
 );
+
+//@route  GET api/profile
+//@desc  Get all profiles
+//@access  Public
+router.get('/',  async (req,res) => {
+    try {
+        const profiles = await Profile.find(),populate('user', ['name', 'avatar']);
+        res.json(profiles);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+        
+    }
+
+})
 module.exports = router;
