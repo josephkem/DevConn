c6a1780f06689f133ac59e45a642040c34edbb77
josephkem <josephkemoy@gmail.com>
2020-06-14 13:29:45 -0400

    Added an auth folder to house the login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Components/layout/Navbar";
 import Landing from "./Components/layout/Landing";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
 import "./App.css";
 
 const App = () => {
