2fced2044eb4e89402986342f561db22ddf98f8a
josephkem <josephkemoy@gmail.com>
2020-08-21 11:08:48 -0400

    Modified the profile actions file to allow the user to create or update a profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -18,3 +18,23 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+//Create or update a profile
+
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {}
+};
