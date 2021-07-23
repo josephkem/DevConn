c1dad3e6bc63c60678bc8c17c4d7d3f2fa5bde7f
josephkem <josephkemoy@gmail.com>
2021-07-23 19:36:16 -0400

    Created files to display education and experiences and added jsx to experience component

---
+++ client/src/Components/Dashboard/Experience.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+
+const Experience = ({ experience }) => {
+  const experiences = experience.map((experience) => (
+    <td key={exp._id}>
+      <td>{exp.compaany}</td>
+      <td className="hide-sm">{exp.title}</td>
+      <td>
+        <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
+        {exp.to === null ? (
+          " Now "
+        ) : (
+          <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+        )}
+      </td>
+      <td>
+        <button className="btn btn-danger">Danger</button>
+      </td>
+    </td>
+  ));
+
+  return (
+    <Fragment>
+      <h2 className="my-2"></h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th className="hide-sm">Titlte</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{experiences}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Experience.propTypes = {};
+
+export default Experience;
