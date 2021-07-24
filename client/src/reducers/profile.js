e493e0a872fa717bb5355b2434154a85bc840f40
josephkem <josephkemoy@gmail.com>
2021-07-24 11:47:02 -0400

    Added functions and actions to get profiles and profiles by id

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,10 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -13,14 +19,19 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
-     case UPDATE_PROFILE: 
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
         loading: false,
       };
-    
-    
+
+    case GET_PROFILES:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
     case PROFILE_ERROR:
       return {
         ...state,
