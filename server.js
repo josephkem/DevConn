f8c0c7606bd58a940eff8b61a1d0f81b63a078e7
josephkem <josephkemoy@gmail.com>
2021-09-01 17:31:12 -0400

    Modified files to prepre for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 //Connect Datbase
 connectDB();
@@ -10,12 +11,20 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("API Running "));
-
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve statuc assets in production
+if (process.env.NODE_ENV == "production") {
+  //set statuc folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
